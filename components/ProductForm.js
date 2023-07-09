f1cae6a15434b5895f00c3da910ca4cf37d49fa4
jeric realubit <jericrealubit@gmail.com>
2023-07-10 03:13:00 +1200

    s3 bucket

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ const ProductForm = ({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) => {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -33,6 +34,24 @@ const ProductForm = ({
     router.push("/products");
   }
 
+  const uploadImages = async (ev) => {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+      // const res = await axios.post("/api/upload", data, {
+      //   headers: { "Content-Type": "multipart/form-data" },
+      // });
+      console.log(res);
+    }
+  };
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -42,6 +61,28 @@ const ProductForm = ({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
