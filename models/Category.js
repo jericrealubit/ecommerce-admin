a24127c9642f5b6b5ed09b3a7f77730fbbcd40f7
jeric realubit <jericrealubit@gmail.com>
2023-07-12 00:03:25 +1200

    adding category to a product

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,8 @@
-import { Schema, models } from "mongoose";
+import mongoose, { Schema, models, model } from "mongoose";
 
 const CategorySchema = new Schema({
   name: { type: String, required: true },
+  parent: { type: mongoose.Types.ObjectId, ref: "Category" },
 });
 
 export const Category = models?.Category || model("Category", CategorySchema);
--- pages/categories.js
+++ pages/categories.js
@@ -1,19 +1,70 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-const categories = () => {
+const Categories = ({ swal }) => {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  const fetchCategories = () => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  };
   const saveCategory = async (ev) => {
     ev.preventDefault();
-    await axios.post("/api/categories", { name });
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
     setName("");
+    setParentCategory("");
+    fetchCategories();
+  };
+
+  const editCategory = (category) => {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id || "");
+  };
+
+  const deleteCategory = (category) => {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete "${category.name}" category?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete!",
+        confirmButtonColor: "#d55",
+        reverseButtons: true,
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          const { _id } = category;
+          await axios.delete(`/api/categories/?_id=${_id}`);
+          fetchCategories();
+        }
+      });
   };
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New category name</label>
+      <label>
+        {editedCategory
+          ? `Edit category ${editedCategory.name}`
+          : "Create new category"}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
           className="mb-0"
@@ -22,12 +73,56 @@ const categories = () => {
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+        <select
+          className="mb-0"
+          value={parentCategory}
+          onChange={(ev) => setParentCategory(ev.target.value)}
+        >
+          <option value="">No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <button type={"Submit"} className="btn-primary py-1">
           Save
         </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent category</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr>
+                <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className="btn-primary mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className="btn-primary"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 };
 
-export default categories;
+//export default categories;
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
