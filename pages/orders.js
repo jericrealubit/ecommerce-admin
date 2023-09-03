9db651160ad4fab62212ae3d5fb868594c8c4815
jeric realubit <jericrealubit@gmail.com>
2023-09-03 22:33:27 +1200

    orders

--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
 import Layout from "/components/Layout";
+import axios from "axios";
 
-const Orders = () => {
-  return <Layout>Orders page here</Layout>;
+const OrdersPage = () => {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => setOrders(response.data));
+  }, []);
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table class="basic">
+        <thead>
+          <th>Date</th>
+          <th>Recipient</th>
+          <th>Products</th>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td>
+                  {order.name} {order.email} <br />
+                  {order.city} {order.postalCode} {order.country} <br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data.product_data.name} ={" "}
+                      {l.price_data.unit_amount / 100} X {l.quantity} <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 };
 
-export default Orders;
+export default OrdersPage;
